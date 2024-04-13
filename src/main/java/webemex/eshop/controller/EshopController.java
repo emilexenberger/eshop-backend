d1d2e36b2472ba860976988f73b7369e6b48e023
emil.exenberger <emil.exenberger@gmail.com>
2024-04-13 14:36:35 +0200

    Clear the project

--- src/main/java/webemex/eshop/controller/EshopController.java
+++ src/main/java/webemex/eshop/controller/EshopController.java
@@ -1,22 +1,21 @@
 package webemex.eshop.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import webemex.eshop.model.Item;
 import webemex.eshop.service.*;
 
-@Controller
+import java.util.List;
+
+@RestController
+@CrossOrigin
 @RequestMapping("/eshop")
 public class EshopController {
     @Autowired
     ItemService itemService;
 
-    @GetMapping("/")
-    @PreAuthorize("isAuthenticated()")
-    public String eshop(Model model) {
-        model.addAttribute("allItems", itemService.findAllItems());
-        return "eshop";
+    @GetMapping("/items")
+    public List<Item> getAllItems() {
+        return itemService.findAllItems();
     }
 }
--- src/main/java/webemex/eshop/controller/IndexController.java
+++ src/main/java/webemex/eshop/controller/IndexController.java
@@ -1,25 +1,24 @@
 package webemex.eshop.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import webemex.eshop.model.AppUser;
-import webemex.eshop.service.AppUserService;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@CrossOrigin
 public class IndexController {
     @Autowired
-    AppUserService appUserService;
 
     @GetMapping("/")
-    public String index(Model model) {
-        AppUser appUser = appUserService.getAuthenticatedUser();
-        if (appUser != null) {
-            model.addAttribute("appUser", appUser);
-        }
-        model.addAttribute("userLogged", appUser != null);
-        model.addAttribute("roleAdmin", appUser != null && appUser.getRole().equals("ROLE_ADMIN"));
-        return "index";
+    public Map<String, Object> index() {
+        Map<String, Object> response = new HashMap<>();
+
+        response.put("userLogged", false);
+
+        return response;
     }
 }
