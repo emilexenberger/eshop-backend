166eeb5990e355a972a62b66c7897629b128b4dd
emil.exenberger <emil.exenberger@gmail.com>
2024-04-17 12:03:02 +0200

    UserManagementService

--- src/main/java/webemex/eshop/controller/IndexController.java
+++ src/main/java/webemex/eshop/controller/IndexController.java
@@ -1,9 +1,13 @@
 package webemex.eshop.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+import webemex.eshop.dto.ReqRes;
+import webemex.eshop.model.OurUsers;
+import webemex.eshop.service.UsersManagementService;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,6 +15,9 @@ import java.util.Map;
 @RestController
 @CrossOrigin
 public class IndexController {
+    @Autowired
+    private UsersManagementService usersManagementService;
+
     @GetMapping("/")
     public Map<String, Object> index() {
         Map<String, Object> response = new HashMap<>();
@@ -18,5 +25,45 @@ public class IndexController {
         response.put("userLogged", true);
 
         return response;
+    }
+
+    @PostMapping("/auth/login")
+    public ResponseEntity<ReqRes> login(@RequestBody ReqRes req){
+        return ResponseEntity.ok(usersManagementService.login(req));
+    }
+
+    @PostMapping("/auth/refresh")
+    public ResponseEntity<ReqRes> refreshToken(@RequestBody ReqRes req){
+        return ResponseEntity.ok(usersManagementService.refreshToken(req));
+    }
+
+    @GetMapping("/admin/get-all-users")
+    public ResponseEntity<ReqRes> getAllUsers(){
+        return ResponseEntity.ok(usersManagementService.getAllUsers());
+
+    }
+
+    @GetMapping("/admin/get-users/{userId}")
+    public ResponseEntity<ReqRes> getUSerByID(@PathVariable Integer userId){
+        return ResponseEntity.ok(usersManagementService.getUsersById(userId));
+
+    }
+
+    @PutMapping("/admin/update/{userId}")
+    public ResponseEntity<ReqRes> updateUser(@PathVariable Integer userId, @RequestBody OurUsers reqres){
+        return ResponseEntity.ok(usersManagementService.updateUser(userId, reqres));
+    }
+
+    @GetMapping("/adminuser/get-profile")
+    public ResponseEntity<ReqRes> getMyProfile(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+        ReqRes response = usersManagementService.getMyInfo(email);
+        return  ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
+    @DeleteMapping("/admin/delete/{userId}")
+    public ResponseEntity<ReqRes> deleteUSer(@PathVariable Integer userId){
+        return ResponseEntity.ok(usersManagementService.deleteUser(userId));
     }
 }
